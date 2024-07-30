004f20a80087076ffaef12db12bcec9bb23195f6
nitishksingh24 <nitishksingh24@gmail.com>
2024-07-30 09:40:22 +0530

    initial commit

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
