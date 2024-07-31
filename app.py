afdc371d06febbcf6f2938088aa7b7f48a535905
nitishksingh24 <nitishksingh24@gmail.com>
2024-07-31 09:26:04 +0530

    add lint check3

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-# add.py
-
-def add(a, b):
-    return a + b
+def add(a,b):
+    return a+b
 
 
-def sub(a, b):
 
-    return a - b
+def sub(a,b): return a-b
+def multiplyNum(a,b):
+    return a*b
+
+result1 = add(2,3)
+result2 = sub(3,1)
+result3 = multiplyNum(2,3)
+
+print(result1,result2,result3)
 
 
-add(4, 5)
-
-sub(3, 1)
