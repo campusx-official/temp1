6473a9a05d39925ea85dc3f04016d5223a8b7f59
nitishksingh24 <nitishksingh24@gmail.com>
2024-07-31 08:47:15 +0530

    add lint check

--- app.py
+++ app.py
@@ -3,8 +3,13 @@
 def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = add(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
+
+
+def sub(a, b):
+
+    return a - b
+
+
+add(4,5)
+
+sub(3,1)
